e9bbaa1d5fb706278e78f4d9afbc6e51a4c935e0
Mallyy <marie.baey@etu.imt-lille-douai.fr>
2021-02-16 16:05:54 +0100

    exo5 update
    grille carré et changmt de taille de la grille

--- tp2/lib/EXO5.dart
+++ tp2/lib/EXO5.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+
 class Exo5 extends StatefulWidget {
   // Declare a field that holds the Todo.
 
@@ -11,6 +14,8 @@ class Exo5 extends StatefulWidget {
 
 ///le state du stateful Widget au dessus
 class _Exo5 extends State<Exo5> {
+  var nbTile= 9;
+  double _sideSquare= 3;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -18,8 +23,67 @@ class _Exo5 extends State<Exo5> {
         appBar: AppBar(
           title: Text('Exo5'),
         ),
-        body: const Image(
-          image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            new Expanded(
+                child:GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 2,
+                mainAxisSpacing: 2,
+                crossAxisCount: _sideSquare.toInt(),
+                children: List.generate(nbTile, (index) {
+                  return Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.black12,
+                      child: Text('Tile $index',)
+                  );
+                })
+            ),
+            ),
+            Slider(
+              value: _sideSquare,
+              min: 2,
+              max: 10,
+              divisions: 8,
+              label: _sideSquare.round().toString(),
+              onChanged: (double value) {
+                setState(() {
+                  _sideSquare = value;
+                  nbTile = _sideSquare.toInt()*_sideSquare.toInt();
+                });
+              },
+            )
+            /*GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 2,
+                mainAxisSpacing: 2,
+                crossAxisCount: 3,
+                children: List.generate(nbTile, (index) {
+                  return Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.black12,
+                      child: Text('Tile $index',)
+                  );
+                })
+            ),
+            Slider(
+              value: _rotateXSliderValue,
+              min: 0,
+              max: 100,
+              divisions: 10,
+              label: _rotateXSliderValue.round().toString(),
+              onChanged: (double value) {
+                setState(() {
+                  _rotateXSliderValue = value;
+                });
+              },
+
+            ),*/
+          ],
         )
     );
     throw UnimplementedError();
